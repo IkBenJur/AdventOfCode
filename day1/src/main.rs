8f885c4589b1291cd126ddc3ef7c24846e9c38a0
IkBenJur <jurspammer@gmail.com>
2023-12-29 12:04:15 +0100

    Fixed edgecase twone. Regex returns 2 but should return 2 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -59,41 +60,60 @@ fn find_first_and_last_digit(calibration_value: &String) -> i32 {
 }
 
 fn find_first_and_last_written_digit(calibration_value: &String) -> i32 {
-    let re = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
-    let digits: Vec<&str> = re
-        .find_iter(calibration_value)
-        .map(|m| m.as_str())
-        .collect();
+    let mut digits: Vec<i32> = Vec::new();
 
+    for i in 0..calibration_value.len() {
+        let string_check = check_if_string_starts_with_digit(&calibration_value[i..]);
+        if let Some(digit) = string_check {
+            digits.push(digit);
+        }
+    }
+    
     let sum_of_first_and_last: String;
     if digits.len() == 1 {
         sum_of_first_and_last = format!(
             "{}{}",
-            written_digit_to_integer(digits[0]),
-            written_digit_to_integer(digits[0])
+            digits[0],
+            digits[0]
         );
     } else {
         sum_of_first_and_last = format!(
             "{}{}",
-            written_digit_to_integer(digits[0]),
-            written_digit_to_integer(digits.last().expect("No last value found"))
+            digits[0],
+            digits.last().expect("No last value found")
         );
     }
 
     return sum_of_first_and_last.parse::<i32>().unwrap();
 }
 
-fn written_digit_to_integer(written_digit: &str) -> i32 {
-    match written_digit {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => written_digit.parse::<i32>().unwrap(),
+fn check_if_string_starts_with_digit(string_slice: &str) -> Option<i32> {
+    let digit_strings = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+
+    for (key, value) in digit_strings.iter() {
+        if string_slice.starts_with(key) {
+            return Some(*value);
+        }
     }
+    
+    return None;
 }
