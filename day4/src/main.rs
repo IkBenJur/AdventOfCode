cacc97e4e87592ac33702e8ad6700f87464ee9c8
IkBenJur <jurspammer@gmail.com>
2024-01-05 20:58:13 +0100

    Day 4 Solution 2. Optimazation: number_of_copies to <i32, i32>

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,10 +1,11 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Scratchcard {
     card_number: i32,
     winning_numbers: Vec<i32>,
@@ -34,6 +35,18 @@ impl Scratchcard {
         }
 
         return score;
+    }
+
+    fn calculate_winning_numbers(&self) -> i32 {
+        let mut winning_numbers = 0;
+
+        for winning_num in &self.winning_numbers {
+            if self.own_numbers.iter().any(|number| number == winning_num) {
+                winning_numbers += 1
+            }
+        }
+
+        return winning_numbers;
     }
 }
 
@@ -78,10 +91,43 @@ fn load_file_into_vector(filename: impl AsRef<Path>) -> Vec<Scratchcard> {
         .collect()
 }
 
+fn copies_of_scratchcards(cards: &Vec<Scratchcard>) -> i32 {
+    let mut number_of_copies: HashMap<i32, Vec<Scratchcard>> = HashMap::new();
+
+    for card in cards {
+        number_of_copies.insert(card.card_number, vec![card.clone()]);
+    }
+
+    let mut current_card_number = 1;
+    loop {
+        if let Some(card_copies) = number_of_copies.get(&current_card_number).cloned() {
+            for card in card_copies {
+                let amount_of_winning_numbers = card.calculate_winning_numbers();
+
+                let card_number_to_increment_to = card.card_number + amount_of_winning_numbers + 1;
+
+                for card_number in card.card_number + 1..card_number_to_increment_to {
+                    // println!("{}", card_number);
+                    let copies_of_card_number = number_of_copies
+                        .entry(card_number)
+                        .or_insert(vec![cards[(card_number - 1) as usize].clone()]);
+                    copies_of_card_number.push(cards[(card_number - 1) as usize].clone());
+                }
+            }
+        } else {
+            break;
+        }
+        current_card_number += 1;
+    }
+
+    return number_of_copies.values().map(|v| v.len() as i32).sum();
+}
+
 fn main() {
     let scratchcards = load_file_into_vector("./src/input1.txt");
 
     let mut sum = 0;
+    let amount_of_scratchcards = copies_of_scratchcards(&scratchcards);
 
     for card in scratchcards {
         if let Some(score) = card.calculate_score() {
@@ -90,4 +136,10 @@ fn main() {
     }
 
     println!("Solution 1: {}", sum);
+
+    //HashMap to keep track of cards and copies <CardNum, CopyNumbers>
+    //CardNum om entry aan te maken als nog niet bestaat anders toevoegen aan copynum
+    //While score not none
+
+    println!("Solution 2: {}", amount_of_scratchcards);
 }
